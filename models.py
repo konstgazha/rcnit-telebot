73fd89333765983b4d909a24405c5bdab7053605
Konstantin Gazha <konstgazha@yandex.ru>
2018-04-06 23:42:24 +0500

    Improve relations between organizations and departments

--- models.py
+++ models.py
@@ -6,25 +6,20 @@ from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
-# organization_department_association = Table('organization_department', Base.metadata,
-#     Column('organization_id', Integer, ForeignKey('organization.id')),
-#     Column('department_id', Integer, ForeignKey('department.id'))
-# )
+
 class OrgDepAssociation(Base):
     __tablename__ = 'org_dep_association'
-    id = Column(Integer, primary_key=True, unique=True)
-    org_id = Column(Integer, ForeignKey('organization.id'), primary_key=True)
-    dep_id = Column(Integer, ForeignKey('department.id'), primary_key=True)
+    id = Column(Integer, primary_key=True)
+    organization_id = Column(Integer, ForeignKey('organization.id'))
+    department_id = Column(Integer, ForeignKey('department.id'))
     employee = relationship("Employee")
-    # employee = relationship("Employee", secondary='employee')
 
 
 class Organization(Base):
     __tablename__ = 'organization'
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    department = relationship("Department", secondary='org_dep_association')
-    # department = relationship("Department", back_populates="organization")
+    org_dep_association = relationship("OrgDepAssociation")
     date_added = Column(DateTime(timezone=True), server_default=func.now())
     date_modified = Column(DateTime(timezone=True), onupdate=func.now())
 
@@ -37,8 +32,7 @@ class Department(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String)
     employee = relationship("Employee")
-    organization = relationship("Organization", secondary='org_dep_association')
-    # organization = relationship("Organization", back_populates="department")
+    org_dep_association = relationship("OrgDepAssociation")
     date_added = Column(DateTime(timezone=True), server_default=func.now())
     date_modified = Column(DateTime(timezone=True), onupdate=func.now())
 

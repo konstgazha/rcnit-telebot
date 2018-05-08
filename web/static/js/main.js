d045a8f2d4e6507e3c04bc43d15db6e9f74b5d7c
Konstantin Gazha <konstgazha@yandex.ru>
2018-05-08 23:39:31 +0500

    Display departments and employees

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -2,10 +2,18 @@ $('.header a').click(function(){
     $.getJSON($SCRIPT_ROOT + '/_get_org_phonebook', {
       org: $(this).text()
     }, function(data) {
-      let deps = []
       for (var i = data.length - 1; i >= 0; i--) {
-          deps += data[i].dep + ' ';
+          let dep = document.createElement("div");
+          document.body.appendChild(dep);
+          let emps = document.createElement("div");
+          document.body.appendChild(emps);
+          dep.innerHTML = data[i].dep;
+          for (var j = data[i].emps.length - 1; j >= 0; j--) {
+            emps.innerHTML += data[i].emps[j].name + ' ' +
+                             data[i].emps[j].surname + ' ' +
+                             data[i].emps[j].patronymic + '<br>';
+          }
       }
-      document.getElementById("phonebook").innerHTML = deps;
+      
     });
 });

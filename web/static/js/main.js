75154c2bd0fbb7fe187578db614b0333fc446acf
Konstantin Gazha <konstgazha@yandex.ru>
2018-05-09 12:52:05 +0500

    Add the table for displaying the phonebook

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -2,18 +2,38 @@ $('.header a').click(function(){
     $.getJSON($SCRIPT_ROOT + '/_get_org_phonebook', {
       org: $(this).text()
     }, function(data) {
+      var phonebook = document.createElement("TABLE");
+      phonebook.setAttribute("id", "phonebook");
+      document.body.appendChild(phonebook);
       for (var i = data.length - 1; i >= 0; i--) {
-          let dep = document.createElement("div");
-          document.body.appendChild(dep);
-          let emps = document.createElement("div");
-          document.body.appendChild(emps);
+          let dep = document.createElement("tr");
+          dep.className = "departments";
+          document.getElementById("phonebook").appendChild(dep);
           dep.innerHTML = data[i].dep;
           for (var j = data[i].emps.length - 1; j >= 0; j--) {
-            emps.innerHTML += data[i].emps[j].name + ' ' +
-                             data[i].emps[j].surname + ' ' +
-                             data[i].emps[j].patronymic + '<br>';
+            let emp = document.createElement("tr");
+            emp.className = "employees";
+            document.getElementById("phonebook").appendChild(emp);
+            // let fullName = document.createElement("td");
+            // fullName.className = "full_name";
+            // emp.appendChild(fullName);
+            // fullName.innerHTML = getEmployeeFullName(data[i].emps[j]);
+            appendTableCell(emp, "full_name", getEmployeeFullName(data[i].emps[j]));
+            appendTableCell(emp, "position", data[i].emps[j].position);
+            appendTableCell(emp, "phone_number", data[i].emps[j].phone_number);
           }
       }
-      
     });
+    function appendTableCell(row, className, value) {
+      let cell = document.createElement("td");
+      cell.className = className;
+      row.appendChild(cell);
+      cell.innerHTML = value;
+    }
+    function getEmployeeFullName(data) {
+      var employee = data.name + ' ' +
+                     data.surname + ' ' +
+                     data.patronymic;
+      return employee;
+    }
 });

4d0166f95d1cb1c9481d74e613798cb7b20366d5
Sanches1408 <Sanches1408@gmail.com>
2018-09-10 09:48:22 +0500

    global update js files

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -2,20 +2,22 @@ $(document).ready(function(){
   $('#header button').click(function() {
     var ind = $(this).text();
     if (window.tableAjax && ind in window.tableAjax) {
-      $('#phonebook').html(window.tableAjax[ind].html());
+      $('#phonebook').remove();
+      $('#main').append($(window.tableAjax[ind]));
     } else {
       $.getJSON($SCRIPT_ROOT + '/_get_org_phonebook', {
         org: ind
       }, function(content) {
-        $('#phonebook').remove();
+        window.content = content;
         $('#main').append(phonebook = $('<table>').attr('id', 'phonebook'));
         phonebook.append($('<thead>').append(tr = $('<tr>')));
         $.each(content.header, function(){
           tr.append($('<th>').text(this));
         });
-        $.each(content.data, function(i){
-          let attr = {'class': 'department', 'rowspan': this.emps.length+1+''};
-          phonebook.append($('<tr>').append($('<th>').attr(attr).text(this.dep)));
+        $.each(content.data, function(){
+          var dep = this.dep;
+          let attr = {'class': 'department', 'rowspan': this.emps.length+1+''}, firstRow = true;
+          phonebook.append(tr = $('<tr>').append($('<td>').attr(attr).text(dep)));
           $.each(this.emps, function(){
             let emps = {
               'position': this.position,
@@ -23,17 +25,24 @@ $(document).ready(function(){
               'phone_number': this.phone_number,
               'internal_phone_number': this.internal_phone_number,
               'email': this.email
-            }, tr = $('<tr>');
+            }, trNew = $('<tr>');
             for (let key in emps) {
-              tr.append(function(){
-                let td = $('<span>').text(emps[key] || '');
-                if (key == 'email') {
-                  td = emps[key] ? $('<a>').attr('href', 'mailto: '+emps[key]).text(emps[key]) : '';
+              $(function(){
+                if (firstRow) {
+                  firstRow = false;
+                  return tr;
+                } else {
+                  phonebook.append(trNew);
+                  return trNew;
                 }
-                return $('<td>').attr('class', key).append(td);
-              });
+              }()).append(function(){
+                let span = $('<span>').text(emps[key] || '');
+                if (key == 'email') {
+                  span = emps[key] ? $('<a>').attr('href', 'mailto: '+emps[key]).text(emps[key]) : '';
+                }
+                return $('<td>').attr('class', key).append(span);
+              }).attr('dep', dep);
             }
-            phonebook.append(tr);
           });
         });
         if (!window.tableAjax)
---
+++ web/static/js/ogbu.js
@@ -0,0 +1,64 @@
+;(function(){
+
+  function ogbu() {
+
+  }
+
+  function search(text) {
+    let arr = [null, false];
+    $('tbody tr').each(function(){
+      var dep = $(this).attr('dep');
+      if (!arr[0]) {
+        arr[0] = dep;
+      } else if (arr[0] != dep) {
+        let depList = '[dep="'+arr[0]+'"]';
+        if (ogbu.search.result(arr[0], text)) {
+          $(depList).show();
+        }
+        depList = $(depList+':visible');
+        let attr = {'class': 'department', 'rowspan': depList.length+''};
+        $('[dep="'+arr[0]+'"] .department').remove();
+        $(depList[0]).prepend($('td').attr(attr).text(arr[0]));
+        arr[0] = null;
+      }
+      $('td', this).each(function(){
+        if (ogbu.seatch.result($(this).text(), text)) {
+          arr[1] = true;
+          ogbu.search.draw();
+        } else {
+          ogbu.search.drawClear();
+        }
+      });
+      if (arr[1]) {
+        $(this).show();
+        arr[1] = false;
+      } else {
+        $(this).hide();
+      }
+    });
+  }
+
+  function result(str, searchStr) {
+    if (str.trim().toLowerCase().search(searchStr.trim().toLowerCase()) > -1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function draw() {
+    console.log('Разукрасили в %cжелтый', 'color: gold;font-weight:bold;');
+  }
+
+  function drawClear() {
+    console.log('Убираем выделение');
+  }
+
+  search.result = result;
+  search.draw = draw;
+  search.drawClear = drawClear;
+
+  ogbu.search = search;
+
+  window.ogbu = ogbu;
+}());

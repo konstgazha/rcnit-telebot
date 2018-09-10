95813975fac2e73562e99fd3da66d14a7b92d706
Sanches1408 <Sanches1408@gmail.com>
2018-09-10 10:50:26 +0500

    insert delete table for change and added correct create cell 'td'

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   $('#header button').click(function() {
-    var ind = $(this).text();
+    var ind = $(this).text().trim();
+    $('#phonebook').remove();
     if (window.tableAjax && ind in window.tableAjax) {
-      $('#phonebook').remove();
       $('#main').append($(window.tableAjax[ind]));
     } else {
       $.getJSON($SCRIPT_ROOT + '/_get_org_phonebook', {
@@ -25,23 +25,16 @@ $(document).ready(function(){
               'phone_number': this.phone_number,
               'internal_phone_number': this.internal_phone_number,
               'email': this.email
-            }, trNew = $('<tr>');
+            }, tr = firstRow ? tr : $('<tr>').attr('dep', dep);
+            firstRow = false;
             for (let key in emps) {
-              $(function(){
-                if (firstRow) {
-                  firstRow = false;
-                  return tr;
-                } else {
-                  phonebook.append(trNew);
-                  return trNew;
-                }
-              }()).append(function(){
+              $(tr).append(function(){
                 let span = $('<span>').text(emps[key] || '');
                 if (key == 'email') {
                   span = emps[key] ? $('<a>').attr('href', 'mailto: '+emps[key]).text(emps[key]) : '';
                 }
                 return $('<td>').attr('class', key).append(span);
-              }).attr('dep', dep);
+              });
             }
           });
         });
